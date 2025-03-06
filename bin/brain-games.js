796efa131203971d2c6e4860fdb487fa8ffa64fe
TechnoBlue12 <artemfun100@gmail.com>
2025-03-06 12:09:51 +0300

    booooo

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,5 @@
-import readlineSync from 'readline-sync';
+import cli from "../src/cli.js"
 
 console.log("Welcome to the Brain Games!")
 
-const name = readlineSync.question('May I have your name? ');
-
-console.log(`Hello, ${name}!`)
-
+cli()
